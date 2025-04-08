847a32803596a63d024ecacf524a2e2d9b9d52eb
ivy159205 <haiduc159205.hsr@outlook.com>
2025-04-08 20:50:39 +0700

    fix lỗi tồn đọng

--- EnglishLearningWebsiteAPI/EnglishLearningWebsite1/EnglishLearningWebsite1/obj/Debug/net8.0/EnglishLearningWebsite1.AssemblyInfo.cs
+++ EnglishLearningWebsiteAPI/EnglishLearningWebsite1/EnglishLearningWebsite1/obj/Debug/net8.0/EnglishLearningWebsite1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnglishLearningWebsite1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f104b646449a587f4564824466a9e8272abd4bae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnglishLearningWebsite1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnglishLearningWebsite1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
